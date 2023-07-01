14e7ea13853a43bb0475e9ca2f58eb611abed104
automation-craftsman <aliulislamabir@gmail.com>
2023-07-02 04:47:00 +0600

    playing with read and write.

---
+++ src/test/java/CreateAndWriteExcelData.java
@@ -0,0 +1,65 @@
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CreateAndWriteExcelData {
+
+    @Test(description = "Creating and storing data in the Excel Sheet.")
+    public void createExcelFile() {
+
+        XSSFWorkbook wb = new XSSFWorkbook();
+        XSSFSheet sheet = wb.createSheet("User_Data");
+
+        Object userData[][] = {
+                {"User_Name", "Password", "Employee_Status"},
+                {"Steve", "Steve123", "Permanent"},
+                {"David", "DavidJ063", "Contractual"},
+                {"Joseph", "Joseph639", "Contractual"},
+                {"Oscar", "OscarR523", "Trainee"}
+        };
+
+        int rowCount = 0;
+
+        for (Object user[] : userData) {
+
+            XSSFRow dataRow = sheet.createRow(rowCount++);
+
+            int columnCount = 0;
+
+            for (Object data : user) {
+
+                XSSFCell cellData = dataRow.createCell(columnCount++);
+
+                if (data instanceof String) cellData.setCellValue((String) data);
+                if (data instanceof Integer) cellData.setCellValue((Integer) data);
+                if (data instanceof Boolean) cellData.setCellValue((Boolean) data);
+
+            }
+
+        }
+
+        String filePath = ".\\src\\test\\resources\\userdata.xlsx";
+
+        FileOutputStream fos = null;
+
+        try {
+            fos = new FileOutputStream(filePath);
+        } catch (FileNotFoundException e) {
+            System.out.println("[!] Error creating file in the system.");
+        }
+
+        try {
+            wb.write(fos);
+        } catch (IOException e) {
+            System.out.println("[!] Error writing file in the system.");
+        }
+
+
+    }
+}
---
+++ src/test/java/ReadExcelData.java
@@ -0,0 +1,51 @@
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class ReadExcelData {
+
+    @Test(description = "Reading data from Excel Sheet")
+    public void readFormExcelFile() throws IOException {
+        String dataFilePath = ".\\src\\test\\resources\\userdata.xlsx";
+
+        // Reading the Excel Workbook
+        XSSFWorkbook wb = new XSSFWorkbook(dataFilePath);
+
+        // Reading the Excel Worksheet
+        XSSFSheet dataSheet = wb.getSheet("User_Data");
+
+        // Number of Rows and Columns in the Worksheet
+        int rows = dataSheet.getLastRowNum();
+        System.out.println("Number of rows: " + rows);
+
+        int cols = dataSheet.getRow(1).getLastCellNum();
+        System.out.println("Number of columns: " + cols);
+
+        for (int r=0; r<=rows; r++){
+
+            XSSFRow selectedRow = dataSheet.getRow(r);
+
+            for (int c=0; c<cols; c++){
+
+                XSSFCell selectedCell = selectedRow.getCell(c);
+
+                switch (selectedCell.getCellType()){
+                    case STRING:
+                        System.out.print(selectedCell.getStringCellValue() + " ");
+                        break;
+                    case NUMERIC:
+                        System.out.print(selectedCell.getNumericCellValue() + " ");
+                        break;
+                    case BOOLEAN:
+                        System.out.print(selectedCell.getBooleanCellValue());
+                        break;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
